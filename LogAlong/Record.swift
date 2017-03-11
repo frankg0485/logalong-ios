c309411718456309e6d12e7318a3e3e4ce4032da
Frank <frank@swoag.com>
2017-03-11 13:07:37 -0600

    storyboard and data class added
    edited the storyboard a tiny bit
    created a Record class with properties and initializer
    
    got an error: variable in optional binding requires initializer

---
+++ LogAlong/Record.swift
@@ -0,0 +1,35 @@
+//
+//  Record.swift
+//  LogAlong
+//
+//  Created by Frank Gao on 3/11/17.
+//  Copyright © 2017 Frank Gao. All rights reserved.
+//
+
+import UIKit
+
+class Record {
+    
+    var category: String?
+    var amount: Int
+    var account: String
+    var payee: String?
+    var notes: String?
+    
+    
+    
+    init?(category: String?, amount: Int, account: String, payee: String?, notes: String?) {
+        
+        guard let !account.isEmpty else {
+            return nil
+        }
+        
+        self.category = category ?? ""
+        self.amount = amount
+        self.account = account
+        self.payee = payee ?? ""
+        self.notes = notes ?? ""
+        
+        
+    }
+}
--- SettingsTableViewController.swift
+++ SettingsTableViewController.swift
@@ -13,7 +13,7 @@ class SettingsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableView.tableFooterView = UIView()
+        //tableView.tableFooterView = UIView()
 
         
         // Uncomment the following line to preserve selection between presentations
@@ -29,17 +29,17 @@ class SettingsTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+/*
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 7
     }
-
+*/
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
