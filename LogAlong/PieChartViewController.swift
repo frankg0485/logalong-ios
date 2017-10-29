300210db7643f0910f1844e6e638d3d90802a94e
Frank <frank@swoag.com>
2017-10-29 11:49:41 -0500

    added basic pie chart with accounts and random amounts

--- LogAlong/PieChartViewController.swift
+++ LogAlong/PieChartViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import Charts
 
 class PieChartViewController: UIViewController {
 
+    @IBOutlet weak var pieChartView: PieChartView!
+
+    let accounts = RecordDB.instance.getAccounts()
+    var amounts: [Double] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tabBarController?.tabBar.isHidden = true
+        navigationController?.isNavigationBarHidden = true
+        for _ in accounts {
+            amounts.append((Double(arc4random()) / 0xFFFFFFFF) * (90) + 10)
+        }
 
+        createPieChart(accounts: accounts, values: amounts)
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +33,35 @@ class PieChartViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func createPieChart(accounts: [Account], values: [Double]) {
+        var entries = [PieChartDataEntry]()
 
+        for i in 0..<values.count {
+            let entry = PieChartDataEntry()
+            entry.y = values[i]
+            entry.label = accounts[i].name
+            entries.append(entry)
+        }
+
+        let set = PieChartDataSet(values: entries, label: "")
+
+        var colors: [UIColor] = []
+
+        for _ in 0..<values.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        set.colors = colors
+
+        let data = PieChartData(dataSet: set)
+
+        pieChartView.data = data
+        pieChartView.chartDescription?.text = ""
+        pieChartView.legend.formSize = 15
+    }
     /*
      // MARK: - Navigation
 
@@ -33,4 +73,3 @@ class PieChartViewController: UIViewController {
      */
 
 }
-
