79814249994264d721fa1048cc416ea0f756ccec
Michael Gao <mgao@swoag.com>
2017-11-22 12:00:10 -0600

    place-holder user login ui code.

--- LogAlong/views/settings/LoginTimerViewController.swift
+++ LogAlong/views/settings/LoginTimerViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class LoginTimerViewController: UIViewController {
+    let TAG = "LoginTimerViewController"
 
     @IBOutlet weak var okButton: UIButton!
     @IBOutlet weak var connectingLabel: UILabel!
@@ -25,6 +26,20 @@ class LoginTimerViewController: UIViewController {
 
         timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(LoginTimerViewController.updateCountDown), userInfo: nil, repeats: true)
         // Do any additional setup after loading the view.
+
+        LBroadcast.register(LBroadcast.ACTION_LOG_IN, cb: #selector(self.login), listener: self)
+        //TODO: pass in user name and pass here, pass in mode (signup vs login) as well
+        //if (is login mode) then
+        UiRequest.instance.UiLogIn("aa", "aaaa")
+
+        //else (sign up mode)
+        //UiRequest.instance.UiSignIn("aa", "aaaa")
+    }
+
+    @objc func login(notification: Notification) -> Void {
+        LLog.d(TAG, "user logged in")
+        //TODO: user successfully logged in, updat GUI (automatically dismiss this popover and update parent view?)
+        stopCountDown()
     }
 
     override func didReceiveMemoryWarning() {
