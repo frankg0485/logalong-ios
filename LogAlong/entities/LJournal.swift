cd58d8f34db9b80415c6c58c04f32c10613ca54b
Michael Gao <mgao@swoag.com>
2017-12-15 15:41:27 -0800

    create all db tables with category journal support.

--- LogAlong/entities/LJournal.swift
+++ LogAlong/entities/LJournal.swift
@@ -157,29 +157,67 @@ class LJournal {
         }
     }
 
+    private class JLCategory: LCategory, GenericJD {
+        init(category: LCategory) {
+            super.init(id: category.id, gid: category.gid, name: category.name,
+                       create: category.timestampCreate, access: category.timestampAccess)
+        }
+
+        func getId() -> Int64 {
+            return id
+        }
+
+        func getGid() -> Int64 {
+            return gid
+        }
+
+        func getRequestCode() -> UInt16 {
+            return LProtocol.JRQST_ADD_CATEGORY
+        }
+
+        func add_data(_ jdata: LBuffer) -> Bool {
+            //data.putLongAutoInc(category.getPid());
+            jdata.putLongAutoInc(0);
+            let sname = [UInt8](name.utf8)
+            jdata.putShortAutoInc(UInt16(sname.count))
+            jdata.putBytesAutoInc(sname)
+            jdata.setLen(jdata.getOffset());
+            return true;
+        }
+
+        static func fetch(_ jdata: LBuffer) -> JLCategory? {
+            if let category = DBCategory.instance.get(id: Int64(jdata.getLongAutoInc())) {
+                return JLCategory(category: category)
+            } else {
+                return nil
+            }
+        }
+    }
+
     private var accountJournalFlushAction: GenericJournalFlushAction<JLAccount>?
+    private var categoryJournalFlushAction: GenericJournalFlushAction<JLCategory>?
 
     func flush() -> Bool {
         /*
-        if (recordJournalFlushAction == null) {
-            recordJournalFlushAction = new LJournal.RecordJournalFlushAction();
-        }
-        if (scheduleJournalFlushAction == null) {
-            scheduleJournalFlushAction = new LJournal.ScheduleJournalFlushAction();
-        }*/
+         if (recordJournalFlushAction == null) {
+         recordJournalFlushAction = new LJournal.RecordJournalFlushAction();
+         }
+         if (scheduleJournalFlushAction == null) {
+         scheduleJournalFlushAction = new LJournal.ScheduleJournalFlushAction();
+         }*/
         if (accountJournalFlushAction == nil) {
-            accountJournalFlushAction = GenericJournalFlushAction<JLAccount>();
+            accountJournalFlushAction = GenericJournalFlushAction<JLAccount>()
+        }
+        if (categoryJournalFlushAction == nil) {
+            categoryJournalFlushAction = GenericJournalFlushAction<JLCategory>()
         }
         /*
-        if (categoryJournalFlushAction == null) {
-            categoryJournalFlushAction = new LJournal.CategoryJournalFlushAction();
-        }
-        if (tagJournalFlushAction == null) {
-            tagJournalFlushAction = new LJournal.TagJournalFlushAction();
-        }
-        if (vendorJournalFlushAction == null) {
-            vendorJournalFlushAction = new LJournal.VendorJournalFlushAction();
-        }*/
+         if (tagJournalFlushAction == null) {
+         tagJournalFlushAction = new LJournal.TagJournalFlushAction();
+         }
+         if (vendorJournalFlushAction == null) {
+         vendorJournalFlushAction = new LJournal.VendorJournalFlushAction();
+         }*/
 
         let entry = DBJournal.instance.get()
         if (nil == entry) {
@@ -206,24 +244,24 @@ class LJournal {
         let ndata: LBuffer = LBuffer(size: LJournal.MAX_JOURNAL_LENGTH);
         switch (jdata.getShortAutoInc()) {
             /*
-        case LProtocol.JRQST_ADD_RECORD:
-            if (!recordJournalFlushAction.add(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_UPDATE_RECORD:
-            if (!recordJournalFlushAction.update(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_DELETE_RECORD:
-            if (!recordJournalFlushAction.delete(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_ADD_SCHEDULE:
-            if (!scheduleJournalFlushAction.add(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_UPDATE_SCHEDULE:
-            if (!scheduleJournalFlushAction.update(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_DELETE_SCHEDULE:
-            if (!scheduleJournalFlushAction.delete(jdata, ndata)) return false;
-            break;
+             case LProtocol.JRQST_ADD_RECORD:
+             if (!recordJournalFlushAction.add(jdata, ndata)) return false;
+             break;
+             case LProtocol.JRQST_UPDATE_RECORD:
+             if (!recordJournalFlushAction.update(jdata, ndata)) return false;
+             break;
+             case LProtocol.JRQST_DELETE_RECORD:
+             if (!recordJournalFlushAction.delete(jdata, ndata)) return false;
+             break;
+             case LProtocol.JRQST_ADD_SCHEDULE:
+             if (!scheduleJournalFlushAction.add(jdata, ndata)) return false;
+             break;
+             case LProtocol.JRQST_UPDATE_SCHEDULE:
+             if (!scheduleJournalFlushAction.update(jdata, ndata)) return false;
+             break;
+             case LProtocol.JRQST_DELETE_SCHEDULE:
+             if (!scheduleJournalFlushAction.delete(jdata, ndata)) return false;
+             break;
              */
         case LProtocol.JRQST_ADD_ACCOUNT:
             (retVal, newEntry, removeEntry) = accountJournalFlushAction!.add(JLAccount.fetch, jdata, ndata)
@@ -231,34 +269,31 @@ class LJournal {
             (retVal, newEntry, removeEntry) = accountJournalFlushAction!.update(JLAccount.fetch, jdata, ndata)
         case LProtocol.JRQST_DELETE_ACCOUNT:
             (retVal, newEntry, removeEntry) = accountJournalFlushAction!.delete(JLAccount.fetch, jdata, ndata)
-            /*
         case LProtocol.JRQST_ADD_CATEGORY:
-            if (!categoryJournalFlushAction.add(jdata, ndata)) return false;
-            break;
+            (retVal, newEntry, removeEntry) = categoryJournalFlushAction!.add(JLCategory.fetch, jdata, ndata)
         case LProtocol.JRQST_UPDATE_CATEGORY:
-            if (!categoryJournalFlushAction.update(jdata, ndata)) return false;
-            break;
+            (retVal, newEntry, removeEntry) = categoryJournalFlushAction!.update(JLCategory.fetch, jdata, ndata)
         case LProtocol.JRQST_DELETE_CATEGORY:
-            if (!categoryJournalFlushAction.delete(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_ADD_TAG:
-            if (!tagJournalFlushAction.add(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_UPDATE_TAG:
-            if (!tagJournalFlushAction.update(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_DELETE_TAG:
-            if (!tagJournalFlushAction.delete(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_ADD_VENDOR:
-            if (!vendorJournalFlushAction.add(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_UPDATE_VENDOR:
-            if (!vendorJournalFlushAction.update(jdata, ndata)) return false;
-            break;
-        case LProtocol.JRQST_DELETE_VENDOR:
-            if (!vendorJournalFlushAction.delete(jdata, ndata)) return false;
-            break;
+            (retVal, newEntry, removeEntry) = categoryJournalFlushAction!.delete(JLCategory.fetch, jdata, ndata)
+            /*
+             case LProtocol.JRQST_ADD_TAG:
+             if (!tagJournalFlushAction.add(jdata, ndata)) return false;
+             break;
+             case LProtocol.JRQST_UPDATE_TAG:
+             if (!tagJournalFlushAction.update(jdata, ndata)) return false;
+             break;
+             case LProtocol.JRQST_DELETE_TAG:
+             if (!tagJournalFlushAction.delete(jdata, ndata)) return false;
+             break;
+             case LProtocol.JRQST_ADD_VENDOR:
+             if (!vendorJournalFlushAction.add(jdata, ndata)) return false;
+             break;
+             case LProtocol.JRQST_UPDATE_VENDOR:
+             if (!vendorJournalFlushAction.update(jdata, ndata)) return false;
+             break;
+             case LProtocol.JRQST_DELETE_VENDOR:
+             if (!vendorJournalFlushAction.delete(jdata, ndata)) return false;
+             break;
              */
         default:
             break
@@ -290,90 +325,89 @@ class LJournal {
         return true;
     }
 
-    /*
-     public boolean getAllAccounts() {
-     return post(LProtocol.JRQST_GET_ACCOUNTS);
-     }
+    func getAllAccounts() -> Bool {
+        return post(LProtocol.JRQST_GET_ACCOUNTS);
+    }
 
-     public boolean getAllCategories() {
-     return post(LProtocol.JRQST_GET_CATEGORIES);
-     }
+    func getAllCategories() -> Bool {
+        return post(LProtocol.JRQST_GET_CATEGORIES);
+    }
 
-     public boolean getAllTags() {
-     return post(LProtocol.JRQST_GET_TAGS);
-     }
+    func getAllTags() -> Bool {
+        return post(LProtocol.JRQST_GET_TAGS);
+    }
 
-     public boolean getAllVendors() {
-     return post(LProtocol.JRQST_GET_VENDORS);
-     }
+    func getAllVendors() -> Bool {
+        return post(LProtocol.JRQST_GET_VENDORS);
+    }
 
-     public boolean getAllRecords() {
-     //return post(LProtocol.JRQST_GET_RECORDS);
-     return getRecords(null);
-     }
+    func getAllRecords() -> Bool {
+        //return post(LProtocol.JRQST_GET_RECORDS);
+        return getRecords(nil);
+    }
 
-     public boolean getAllSchedules() {
-     return post(LProtocol.JRQST_GET_SCHEDULES);
-     }
+    func getAllSchedules() -> Bool {
+        return post(LProtocol.JRQST_GET_SCHEDULES);
+    }
 
-     public boolean getAccountRecords(long aid) {
-     return postById(aid, LProtocol.JRQST_GET_ACCOUNT_RECORDS);
-     }
+    func getAccountRecords(_ aid: Int64) -> Bool {
+        return postById(aid, LProtocol.JRQST_GET_ACCOUNT_RECORDS);
+    }
 
-     public boolean getAccountSchedules(long aid) {
-     return postById(aid, LProtocol.JRQST_GET_ACCOUNT_SCHEDULES);
-     }
+    func getAccountSchedules(_ aid: Int64) -> Bool {
+        return postById(aid, LProtocol.JRQST_GET_ACCOUNT_SCHEDULES);
+    }
 
-     public boolean getAccountUsers(long aid) {
-     return postById(aid, LProtocol.JRQST_GET_ACCOUNT_USERS);
-     }
+    func getAccountUsers(_ aid: Int64) -> Bool {
+        return postById(aid, LProtocol.JRQST_GET_ACCOUNT_USERS);
+    }
 
-     public boolean getRecord(long id) {
-     return postById(id, LProtocol.JRQST_GET_RECORD);
-     }
+    func getRecord(_ id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_GET_RECORD);
+    }
 
-     public boolean getRecords(long ids[]) {
-     data.clear();
-     data.putShortAutoInc(LProtocol.JRQST_GET_RECORDS);
-     if (null == ids) {
-     data.putShortAutoInc((short) 0); // get all records;
-     } else {
-     data.putShortAutoInc((short) ids.length);
-     for (long id : ids) {
-     data.putLongAutoInc(id);
-     }
-     }
-     data.setLen(data.getBufOffset());
-     return post();
-     }
+    func getRecords(_ ids: [Int64]? ) -> Bool {
+        data.clear();
+        data.putShortAutoInc(LProtocol.JRQST_GET_RECORDS);
+        if (nil == ids) {
+            data.putShortAutoInc(UInt16(0)); // get all records;
+        } else {
+            data.putShortAutoInc(UInt16(ids!.count));
+            for id in ids! {
+                data.putLongAutoInc(id);
+            }
+        }
+        data.setLen(data.getOffset());
+        return post();
+    }
 
-     public boolean addRecord(long id) {
-     return postById(id, LProtocol.JRQST_ADD_RECORD);
-     }
+    func addRecord(id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_ADD_RECORD);
+    }
 
-     public boolean updateRecord(long id) {
-     return postById(id, LProtocol.JRQST_UPDATE_RECORD);
-     }
+    func updateRecord(id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_UPDATE_RECORD);
+    }
 
-     public boolean deleteRecord(long id) {
-     return postById(id, LProtocol.JRQST_DELETE_RECORD);
-     }
+    func deleteRecord(id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_DELETE_RECORD);
+    }
 
-     public boolean getSchedule(long id) {
-     return postById(id, LProtocol.JRQST_GET_SCHEDULE);
-     }
+    func getSchedule(id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_GET_SCHEDULE);
+    }
 
-     public boolean addSchedule(long id) {
-     return postById(id, LProtocol.JRQST_ADD_SCHEDULE);
-     }
+    func addSchedule(id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_ADD_SCHEDULE);
+    }
 
-     public boolean updateSchedule(long id) {
-     return postById(id, LProtocol.JRQST_UPDATE_SCHEDULE);
-     }
+    func updateSchedule(id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_UPDATE_SCHEDULE);
+    }
 
-     public boolean deleteSchedule(long id) {
-     return postById(id, LProtocol.JRQST_DELETE_SCHEDULE);
-     }*/
+    func deleteSchedule(id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_DELETE_SCHEDULE);
+    }
 
     func addAccount(_ id: Int64) -> Bool {
         return postById(id, LProtocol.JRQST_ADD_ACCOUNT);
@@ -387,60 +421,59 @@ class LJournal {
         return postById(id, LProtocol.JRQST_DELETE_ACCOUNT);
     }
 
-    /*
-     public boolean addCategory(long id) {
-     return postById(id, LProtocol.JRQST_ADD_CATEGORY);
-     }
+    func addCategory(_ id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_ADD_CATEGORY);
+    }
 
-     public boolean updateCategory(long id) {
-     return postById(id, LProtocol.JRQST_UPDATE_CATEGORY);
-     }
+    func updateCategory(_ id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_UPDATE_CATEGORY);
+    }
 
-     public boolean deleteCategory(long id) {
-     return postById(id, LProtocol.JRQST_DELETE_CATEGORY);
-     }
+    func deleteCategory(_ id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_DELETE_CATEGORY);
+    }
 
-     public boolean addTag(long id) {
-     return postById(id, LProtocol.JRQST_ADD_TAG);
-     }
+    func addTag(_ id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_ADD_TAG);
+    }
 
-     public boolean updateTag(long id) {
-     return postById(id, LProtocol.JRQST_UPDATE_TAG);
-     }
+    func updateTag(_ id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_UPDATE_TAG);
+    }
 
-     public boolean deleteTag(long id) {
-     return postById(id, LProtocol.JRQST_DELETE_TAG);
-     }
+    func deleteTag(_ id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_DELETE_TAG);
+    }
 
-     public boolean addVendor(long id) {
-     return postById(id, LProtocol.JRQST_ADD_VENDOR);
-     }
+    func addVendor(_ id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_ADD_VENDOR);
+    }
 
-     public boolean updateVendor(long id) {
-     return postById(id, LProtocol.JRQST_UPDATE_VENDOR);
-     }
+    func updateVendor(_ id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_UPDATE_VENDOR);
+    }
 
-     public boolean deleteVendor(long id) {
-     return postById(id, LProtocol.JRQST_DELETE_VENDOR);
-     }
+    func deleteVendor(_ id: Int64) -> Bool {
+        return postById(id, LProtocol.JRQST_DELETE_VENDOR);
+    }
 
-     public boolean addUserToAccount(long uid, long aid) {
-     return postLongLong(uid, aid, LProtocol.JRQST_ADD_USER_TO_ACCOUNT);
-     }
+    func addUserToAccount(uid: Int64,  aid: Int64) -> Bool {
+        return postLongLong(uid, aid, LProtocol.JRQST_ADD_USER_TO_ACCOUNT);
+    }
 
-     public boolean removeUserFromAccount(long uid, long aid) {
-     return postLongLong(uid, aid, LProtocol.JRQST_REMOVE_USER_FROM_ACCOUNT);
-     }
+    func removeUserFromAccount(uid: Int64, aid: Int64) -> Bool {
+        return postLongLong(uid, aid, LProtocol.JRQST_REMOVE_USER_FROM_ACCOUNT);
+    }
 
-     public boolean confirmAccountShare(long aid, long uid, boolean yes) {
-     data.clear();
-     data.putShortAutoInc(LProtocol.JRQST_CONFIRM_ACCOUNT_SHARE);
-     data.putLongAutoInc(aid);
-     data.putLongAutoInc(uid);
-     data.putByteAutoInc((byte) (yes ? 1 : 0));
-     data.setLen(data.getBufOffset());
-     return post();
-     }*/
+    func confirmAccountShare(aid: Int64, uid: Int64, yes: Bool) -> Bool {
+        data.clear();
+        data.putShortAutoInc(LProtocol.JRQST_CONFIRM_ACCOUNT_SHARE);
+        data.putLongAutoInc(aid);
+        data.putLongAutoInc(uid);
+        data.putByteAutoInc(UInt8(yes ? 1 : 0));
+        data.setLen(data.getOffset());
+        return post();
+    }
 
     private func post() -> Bool {
         if LPreferences.getUserId().isEmpty {
--- LogAlong/utils/DBHelper.swift
+++ LogAlong/utils/DBHelper.swift
@@ -13,8 +13,13 @@ class DBHelper {
     let db: Connection?
 
     let accounts = Table("LAccount")
+    let accountBalances = Table("LAccountBalance")
     let categories = Table("LCategory")
+    let tags = Table("LTag")
+    let vendors = Table("LVendor")
+    let vendorCategories = Table("LVendorCategory")
     let transactions = Table("LTransaction")
+    let scheduledTransactions = Table("LScheduledTransactions")
     let journals = Table("LJournal")
 
     static let id = Expression<Int64>("Id")
@@ -22,8 +27,13 @@ class DBHelper {
     static let rid = Expression<Int64>("Rid")
     static let name = Expression<String>("Name")
     static let accountId = Expression<Int64>("AccountId")
+    static let accountId2 = Expression<Int64>("AccountId2")
     static let categoryId = Expression<Int64>("CategoryId")
+    static let tagId = Expression<Int64>("TagId")
+    static let vendorId = Expression<Int64>("VendorId")
     static let amount = Expression<Double>("Amount")
+    static let note = Expression<String>("Note")
+    static let irid = Expression<Int64>("IRid")
     static let timestamp = Expression<Int64>("Timestamp")
     static let timestampCretae = Expression<Int64>("TimestampCreate")
     static let timestampAccess = Expression<Int64>("TimestampAccess")
@@ -31,6 +41,13 @@ class DBHelper {
     static let showBalance = Expression<Int>("ShowBalance")
     static let icon = Expression<Data?>("Icon")
     static let share = Expression<String>("Share")
+    static let enable = Expression<Int>("Enable")
+    static let repeatCount = Expression<Int>("RepeatCount")
+    static let repeatInterval = Expression<Int>("RepeatInterval")
+    static let repeatUnit = Expression<Int>("RepeatUnit")
+    static let scheduleTime = Expression<Int64>("ScheduleTime")
+    static let year = Expression<Int>("Year")
+    static let balance = Expression<String>("Balance")
 
     static let journalId = Expression<Int>("JournalId")
     static let data = Expression<Data>("Data")
@@ -66,13 +83,64 @@ class DBHelper {
         }
 
         do {
+            try db!.run(accountBalances.create(ifNotExists: true) { table in
+                table.column(DBHelper.id, primaryKey: true)
+                table.column(DBHelper.accountId)
+                table.column(DBHelper.year)
+                table.column(DBHelper.balance)
+            })
+        } catch {
+            LLog.e("\(self)", "Unable to create account balances table")
+        }
+
+        do {
             try db!.run(categories.create(ifNotExists: true) { table in
                 table.column(DBHelper.id, primaryKey: true)
                 table.column(DBHelper.gid)
                 table.column(DBHelper.name, unique: true, collate: .nocase)
+                table.column(DBHelper.timestampCretae)
+                table.column(DBHelper.timestampAccess)
+                table.column(DBHelper.icon)
             })
         } catch {
             LLog.e("\(self)", "Unable to create categories table")
+        }
+
+        do {
+            try db!.run(tags.create(ifNotExists: true) { table in
+                table.column(DBHelper.id, primaryKey: true)
+                table.column(DBHelper.gid)
+                table.column(DBHelper.name, unique: true, collate: .nocase)
+                table.column(DBHelper.timestampCretae)
+                table.column(DBHelper.timestampAccess)
+                table.column(DBHelper.icon)
+            })
+        } catch {
+            LLog.e("\(self)", "Unable to create tags table")
+        }
+
+        do {
+            try db!.run(vendors.create(ifNotExists: true) { table in
+                table.column(DBHelper.id, primaryKey: true)
+                table.column(DBHelper.gid)
+                table.column(DBHelper.name, unique: true, collate: .nocase)
+                table.column(DBHelper.type)
+                table.column(DBHelper.timestampCretae)
+                table.column(DBHelper.timestampAccess)
+                table.column(DBHelper.icon)
+            })
+        } catch {
+            LLog.e("\(self)", "Unable to create vendor table")
+        }
+
+        do {
+            try db!.run(vendorCategories.create(ifNotExists: true) { table in
+                table.column(DBHelper.id, primaryKey: true)
+                table.column(DBHelper.vendorId)
+                table.column(DBHelper.categoryId)
+            })
+        } catch {
+            LLog.e("\(self)", "Unable to create vendor categories table")
         }
 
         do {
@@ -80,13 +148,49 @@ class DBHelper {
                 table.column(DBHelper.id, primaryKey: true)
                 table.column(DBHelper.gid)
                 table.column(DBHelper.accountId)
+                table.column(DBHelper.accountId2)
                 table.column(DBHelper.categoryId)
+                table.column(DBHelper.tagId)
+                table.column(DBHelper.vendorId)
                 table.column(DBHelper.amount)
                 table.column(DBHelper.timestamp)
                 table.column(DBHelper.type)
+                table.column(DBHelper.note)
+                table.column(DBHelper.irid)
+                table.column(DBHelper.timestampCretae)
+                table.column(DBHelper.timestampAccess)
+                table.column(DBHelper.icon)
             })
         } catch {
-            LLog.e("\(self)", "Unable to create records table")
+            LLog.e("\(self)", "Unable to create transactions table")
+        }
+
+        do {
+            try db!.run(scheduledTransactions.create(ifNotExists: true) { table in
+                table.column(DBHelper.id, primaryKey: true)
+                table.column(DBHelper.gid)
+                table.column(DBHelper.accountId)
+                table.column(DBHelper.accountId2)
+                table.column(DBHelper.categoryId)
+                table.column(DBHelper.tagId)
+                table.column(DBHelper.vendorId)
+                table.column(DBHelper.amount)
+                table.column(DBHelper.timestamp)
+                table.column(DBHelper.type)
+                table.column(DBHelper.note)
+                table.column(DBHelper.irid)
+                table.column(DBHelper.timestampCretae)
+                table.column(DBHelper.timestampAccess)
+                table.column(DBHelper.icon)
+
+                table.column(DBHelper.enable)
+                table.column(DBHelper.repeatUnit)
+                table.column(DBHelper.repeatCount)
+                table.column(DBHelper.repeatInterval)
+                table.column(DBHelper.scheduleTime)
+            })
+        } catch {
+            LLog.e("\(self)", "Unable to create scheduled transactions table")
         }
 
         do {
