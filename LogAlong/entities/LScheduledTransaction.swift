491cd5b8362012e268a61182eec13327d9b5662c
Michael Gao <mgao@swoag.com>
2017-12-17 19:43:43 -0800

    added scheduled transaction and transaction details class place-holders.

---
+++ LogAlong/entities/LScheduledTransaction.swift
@@ -0,0 +1,13 @@
+//
+//  LScheduledTransaction.swift
+//  LogAlong
+//
+//  Created by Michael Gao on 12/17/17.
+//  Copyright © 2017 Swoag Technology. All rights reserved.
+//
+
+import Foundation
+
+class LScheduledTransaction: LTransaction {
+
+}
