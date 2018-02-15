a180022388a8ea05e0685093371a2acc2bc8ee8b
Michael Gao <mgao@swoag.com>
2018-02-14 18:30:32 -0800

    project and storyboard update for place-holder schedule list/edit GUI.

---
+++ LogAlong/views/SchedulesTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SchedulesTableViewController.swift
+//  LogAlong
+//
+//  Created by Michael Gao on 2/14/18.
+//  Copyright © 2018 Swoag Technology. All rights reserved.
+//
+
+import Foundation
