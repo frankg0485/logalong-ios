06e5228ba4e69bc6e662ce4a34df201571f4dc3d
Michael Gao <mgao@swoag.com>
2018-03-05 17:32:46 -0800

    project and storyboard update for profile edit gui cleanup.

---
+++ LogAlong/utils/LCheckbox.swift
@@ -0,0 +1,9 @@
+//
+//  LCheckbox.swift
+//  LogAlong
+//
+//  Created by Michael Gao on 3/1/18.
+//  Copyright © 2018 Swoag Technology. All rights reserved.
+//
+
+import Foundation
---
+++ LogAlong/views/settings/ProfileViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewController.swift
+//  LogAlong
+//
+//  Created by Michael Gao on 2/28/18.
+//  Copyright © 2018 Swoag Technology. All rights reserved.
+//
+
+import Foundation
